6429682f247d58e48beb354cf32514b9cec91c50
vinsontandraa <vtandra7@gmail.com>
2021-06-17 14:36:20 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import MiningProcess from "./components/MiningProcess";
+
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <MiningProcess />
+    </div>
+  );
+}
+
+export default App;
